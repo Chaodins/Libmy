7eb531369975cce6bd71beb7953df300e7efcf9f
kais <kais.mekdad@epitech.eu>
2019-11-12 11:01:23 +0100

    error gestion in my_put_nbr_base done and associated tests added

--- src/main/my_put_nbr_base.c
+++ src/main/my_put_nbr_base.c
@@ -1,9 +1,15 @@
+#include <unistd.h>
+
 #include "my.h"
 
 int my_put_nbr_base(long nb, char *base)
 {
     int b = my_strlen(base);
 
+    if (base == NULL)
+        return (84);
+    if (b < 2)
+        return (84);
     if (nb < 0) {
         my_putchar('-');
         nb *= -1;
--- src/test/my_put_nbr_base_tests.c
+++ src/test/my_put_nbr_base_tests.c
@@ -24,9 +24,27 @@ Test(my_put_nbr_base, should_print_r)
     cr_assert_stdout_eq_str("r");
 }
 
-Test(my_put_nbr_base, should_print_minus_55)
+Test(my_put_nbr_base, should_print_5)
 {
     cr_redirect_stdout();
     my_put_nbr_base(0, "516");
     cr_assert_stdout_eq_str("5");
+}
+
+Test(my_put_nbr_base, should_print_minus_55)
+{
+    cr_redirect_stdout();
+    my_put_nbr_base(-1, "ab");
+    cr_assert_stdout_eq_str("-b");
+}
+
+Test(my_put_nbr_base, should_return_84_for_invalid_base)
+{
+    cr_assert(my_put_nbr_base(-1, "1") == 84, "should return 84 for an invalid base.");
+    cr_assert(my_put_nbr_base(-1, "") == 84, "should return 84 for an invalid base.");
+}
+
+Test(my_put_nbr_base, should_return_84_for_NULL_base)
+{
+    cr_assert(my_put_nbr_base(-1, NULL) == 84, "should return 84 for a NULL base.");
 }
