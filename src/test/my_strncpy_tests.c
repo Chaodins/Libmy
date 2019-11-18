e3640bac5bfa1c1f8499d6a7a72e80c535bfa8e1
kais <kais.mekdad@epitech.eu>
2019-11-18 16:02:37 +0100

    adding basic tests to my_strncpy

--- src/test/my_strncpy_tests.c
+++ src/test/my_strncpy_tests.c
@@ -3,7 +3,22 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-Test(my_strncpy, expected)
+Test(my_strncpy, expected_to_copy_n_characters_from_src)
 {
+    char src[] = "monsieur";
+    char dest[9];
+    char *res = my_strncpy(dest, src, 8);
 
+    cr_assert_str_eq("monsieur", res);
+    cr_assert_eq(dest, res);
+}
+
+Test(my_strncpy, expected_to_copy_n_characters_from_src_1)
+{
+    char src[] = "monsieur";
+    char dest[4];
+    char *res = my_strncpy(dest, src, 3);
+
+    cr_assert_str_eq("mon", res);
+    cr_assert_eq(dest, res);
 }
