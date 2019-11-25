6ca59f07b80ca7dfd655e1d6212be42586a5a28c
kais <kais.mekdad@epitech.eu>
2019-11-25 11:05:29 +0100

    minor Fixing on my_strcat$

--- src/main/my_strcat.c
+++ src/main/my_strcat.c
@@ -3,9 +3,9 @@
 char *my_strcat(char *dest, char const *src)
 {
     int size = my_strlen(dest);
-    int i;
+    int i = 0;
 
-    for(i = 0; src[i] != '\0'; i++)
+    for(; src[i] != '\0'; i++)
         dest[size + i] = src[i];
     dest[size + i] = '\0';
     return (dest);
