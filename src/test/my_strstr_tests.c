ebd09c6dd9ccff9c9424df3c584af64f6e394b8f
kais <kais.mekdad@epitech.eu>
2019-11-26 09:41:32 +0100

    adding tests to strstr for covering

--- src/test/my_strstr_tests.c
+++ src/test/my_strstr_tests.c
@@ -3,9 +3,16 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-Test(my_strstr, expected)
+Test(my_strstr, expected_to_return_the_string_after_the_patern)
 {
     char str[] = "Bonsoir monsieur le monarque.";
-    
+
     cr_assert_str_eq("monsieur le monarque.", my_strstr(str, "mon"));
+}
+
+Test(my_strstr, expected_to_return_NULL)
+{
+    char str[] = "";
+
+    cr_assert(NULL == my_strstr(str, "mon"), "Should return null.");
 }
