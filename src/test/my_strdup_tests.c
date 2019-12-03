9584ff0e3a5b6f84c5aebd9303cca66006c4a304
kais <kais.mekdad@epitech.eu>
2019-12-03 10:52:46 +0100

    adding my_strdup tests

--- src/test/my_strdup_tests.c
+++ src/test/my_strdup_tests.c
@@ -13,3 +13,33 @@ Test(my_strdup, expected_to_duplicate_the_string)
     cr_assert_str_eq(str, dup);
     free(dup);
 }
+
+Test(my_strdup, expected_to_duplicate_the_3_characters_string)
+{
+    char str[] = "yay";
+    char *dup = my_strdup(str);
+
+    cr_assert(dup != str, "Dup should create a new string.");
+    cr_assert_str_eq(str, dup);
+    free(dup);
+}
+
+Test(my_strdup, expected_to_duplicate_the_characters_string)
+{
+    char str[] = "o";
+    char *dup = my_strdup(str);
+
+    cr_assert(dup != str, "Dup should create a new string.");
+    cr_assert_str_eq(str, dup);
+    free(dup);
+}
+
+Test(my_strdup, expected_to_duplicate_empty_string)
+{
+    char str[] = "";
+    char *dup = my_strdup(str);
+
+    cr_assert(dup != str, "Dup should create a new string.");
+    cr_assert_str_eq(str, dup);
+    free(dup);
+}
