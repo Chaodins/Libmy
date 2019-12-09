6e040c8c2c2fd8e7e48ada45efff41b8017f4196
kais <kais.mekdad@epitech.eu>
2019-12-09 11:20:01 +0100

    Fixing build issue

--- src/test/my_showmem_tests.c
+++ src/test/my_showmem_tests.c
@@ -1,11 +1,9 @@
-// #include "../main/my.h"
-//
-// #include <criterion/criterion.h>
-// #include <criterion/redirect.h>
-//
-// Test(my_showmem, expected)
-// {
-//     cr_redirect_stdout();
-//     my_showmem("AAAAAAAAAAAAAAAA", 16);
-//     cr_assert_stdout_eq_str("00000000:  4141 4141 4141 4141 4141 4141 4141 4141 AAAAAAAAAAAAAAAA");
-// }
+#include "../main/my.h"
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(my_showmem, expected)
+{
+    
+}
