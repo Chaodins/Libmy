288df3f9254b615e0f5813b73687d6334dcc9692
kais <kais.mekdad@epitech.eu>
2019-12-11 16:19:49 +0100

    deleting my_showmem

--- src/main/my_strupcase.c
+++ src/main/my_strupcase.c
@@ -4,8 +4,9 @@
 
 char *my_strupcase(char *str)
 {
-    for (int i = 0; str[i]; i++)
+    for (int i = 0; str[i]; i++) {
         if (str[i] <= 'z' && str[i] >= 'a')
             str[i] -= 32;
+    }
     return (str);
 }
